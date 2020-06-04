d0b0b34aecfd668e6bdcfb296516ad5d3b0ff670
Theodosios Tziomakas <theodosiostziomakas@Theodosioss-MacBook-Pro.local>
2020-06-04 08:32:10 +0300

    Create a Separate Test Database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,12 @@
 import * as firebase from "firebase";
 
 var config = {
-    apiKey: "AIzaSyD7aaHMZmDrnN6zytgBPHNvyddBXocZnMc",
-    authDomain: "expensify-b09a1.firebaseapp.com",
-    databaseURL: "https://expensify-b09a1.firebaseio.com",
-    projectId: "expensify-b09a1",
-    storageBucket: "expensify-b09a1.appspot.com",
-    messagingSenderId: "989743126819",
-    appId: "1:989743126819:web:081b695b26ba8800e05496",
-    measurementId: "G-D14P1ZYP4S"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
