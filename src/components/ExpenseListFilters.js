bfe7912d3904cdeb0f223510f7b5f281a2e41d2e
Theodosios Tziomakas <theodosiostziomakas@Theodosioss-MacBook-Pro.local>
2020-06-04 09:35:34 +0300

    Some changes

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -14,6 +14,7 @@ export class ExpenseListFilters extends React.Component {
   onFocusChange = (calendarFocused) => {
     this.setState(() => ({ calendarFocused }));
   }
+  
   onTextChange = (e) => {
     this.props.setTextFilter(e.target.value);
   };
